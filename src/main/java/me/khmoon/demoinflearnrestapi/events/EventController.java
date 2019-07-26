598a64e13769d46b5aff366abec84ee695148381
kwanghyunMoon <mgh3326@naver.com>
2019-07-26 14:12:01 +0900

    validate wrong data

--- src/main/java/me/khmoon/demoinflearnrestapi/events/EventController.java
+++ src/main/java/me/khmoon/demoinflearnrestapi/events/EventController.java
@@ -1,22 +1,44 @@
 package me.khmoon.demoinflearnrestapi.events;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.net.URI;
 
 @Controller
 @RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_UTF8_VALUE)
 public class EventController {
-  @PostMapping
-  public ResponseEntity createEvent(@RequestBody Event event) {
+  private final EventValidator eventValidator;
+  private final ModelMapper modelMapper;
+  private final EventRepository eventRepository;
 
-    URI createUri = ControllerLinkBuilder.linkTo(EventController.class).slash("{id}").toUri();
+  public EventController(EventRepository eventRepository, ModelMapper modelMapper, EventValidator eventValidator) {
+    this.eventRepository = eventRepository;
+    this.modelMapper = modelMapper;
+    this.eventValidator = eventValidator;
+  }
+
+  @PostMapping
+  public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {
+    if (errors.hasErrors()) {
+      return ResponseEntity.badRequest().build();
+    }
+    eventValidator.validate(eventDto, errors);
+    if (errors.hasErrors()) {
+      return ResponseEntity.badRequest().build();
+    }
+    Event event = modelMapper.map(eventDto, Event.class);
+    Event newEvent = this.eventRepository.save(event);
+    URI createUri = ControllerLinkBuilder.linkTo(EventController.class).slash(newEvent.getId()).toUri();
     event.setId(10);
     return ResponseEntity.created(createUri).body(event);
   }
--- src/test/java/me/khmoon/demoinflearnrestapi/events/EventControllerTest.java
+++ src/test/java/me/khmoon/demoinflearnrestapi/events/EventControllerTest.java
@@ -1,11 +1,18 @@
 package me.khmoon.demoinflearnrestapi.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -13,11 +20,14 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.time.LocalDateTime;
+import java.
+
+        time.LocalDateTime;
 
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 public class EventControllerTest {
   @Autowired
   MockMvc mockMvc;
@@ -27,7 +37,7 @@ public class EventControllerTest {
 
   @Test
   public void createEvent() throws Exception {
-    Event event = Event.builder()
+    EventDto event = EventDto.builder()
             .name("Spring")
             .description("REST API Development with Spring")
             .beginEnrollmentDateTime(LocalDateTime.of(2018, 11, 23, 14, 21))
@@ -47,7 +57,72 @@ public class EventControllerTest {
     )
             .andDo(MockMvcResultHandlers.print())
             .andExpect(MockMvcResultMatchers.status().isCreated())
-            .andExpect(MockMvcResultMatchers.jsonPath("id").exists());
+            .andExpect(MockMvcResultMatchers.jsonPath("id").exists())
+            .andExpect(MockMvcResultMatchers.header().exists(HttpHeaders.LOCATION))
+            .andExpect(MockMvcResultMatchers.header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
+            .andExpect(MockMvcResultMatchers.jsonPath("id").value(Matchers.not(100)))
+            .andExpect(MockMvcResultMatchers.jsonPath("free").value(Matchers.not(true)))
+            .andExpect(MockMvcResultMatchers.jsonPath("eventStatus").value(EventStatus.DRAFT.name()))
+    ;
+  }
 
+  @Test
+  public void createEvent_Bad_Request() throws Exception {
+    Event event = Event.builder()
+            .id(100)
+            .name("Spring")
+            .description("REST API Development with Spring")
+            .beginEnrollmentDateTime(LocalDateTime.of(2018, 11, 23, 14, 21))
+            .closeEnrollmentDateTime(LocalDateTime.of(2018, 11, 24, 14, 21))
+            .beginEventDateTime(LocalDateTime.of(2018, 11, 25, 14, 21))
+            .endEventDateTime(LocalDateTime.of(2018, 11, 26, 14, 21))
+            .basePrice(100)
+            .maxPrice(200)
+            .limitOfEnrollment(100)
+            .location("강남역 D2 스타텁 팩토리")
+            .free(true)
+            .offline(false)
+            .eventStatus(EventStatus.PUBLISHED)
+            .build();
+
+    mockMvc.perform(MockMvcRequestBuilders.post("/api/events")
+            .contentType(MediaType.APPLICATION_JSON_UTF8)
+            .accept(MediaTypes.HAL_JSON)
+            .content(objectMapper.writeValueAsString(event))
+    )
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(MockMvcResultMatchers.status().isBadRequest())
+    ;
+  }
+
+  @Test
+  public void createEvent_Bad_Request_Empty_Input() throws Exception {
+    EventDto eventDto = EventDto.builder().build();
+    this.mockMvc.perform(MockMvcRequestBuilders.post("/api/events")
+            .contentType(MediaType.APPLICATION_JSON_UTF8)
+            .content(this.objectMapper.writeValueAsString(eventDto))
+    )
+            .andExpect(MockMvcResultMatchers.status().isBadRequest());
+  }
+
+  @Test
+  public void createEvent_Bad_Request_Wrong_Input() throws Exception {
+    EventDto eventDto = EventDto.builder()
+            .name("Spring")
+            .description("REST API Development with Spring")
+            .beginEnrollmentDateTime(LocalDateTime.of(2018, 11, 23, 14, 21))
+            .closeEnrollmentDateTime(LocalDateTime.of(2018, 11, 24, 14, 21))
+            .beginEventDateTime(LocalDateTime.of(2018, 11, 25, 14, 21))
+            .endEventDateTime(LocalDateTime.of(2018, 11, 23, 14, 21))
+            .basePrice(10000)
+            .maxPrice(200)
+            .limitOfEnrollment(100)
+            .location("강남역 D2 스타텁 팩토리")
+            .build();
+    this.mockMvc.perform(MockMvcRequestBuilders.post("/api/events")
+            .contentType(MediaType.APPLICATION_JSON_UTF8)
+            .content(this.objectMapper.writeValueAsString(eventDto))
+    )
+            .andExpect(MockMvcResultMatchers.status().isBadRequest());
   }
 }
