96fe4b01a4ec14196493fac3012663ed886ece3b
kwanghyunMoon <mgh3326@naver.com>
2019-08-16 19:12:25 +0900

    Test DB and config

--- src/test/java/me/khmoon/demoinflearnrestapi/events/EventControllerTest.java
+++ src/test/java/me/khmoon/demoinflearnrestapi/events/EventControllerTest.java
@@ -18,6 +18,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -41,6 +42,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
 @Import(RestDocsConfiguration.class)
+@ActiveProfiles("test")
 public class EventControllerTest {
   @Autowired
   MockMvc mockMvc;
